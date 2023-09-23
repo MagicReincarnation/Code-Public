63b9c12cbf35715126377dcf44ad6f073f284e8a
MAGIC Reincarnated <139699919+MagicReincarnation@users.noreply.github.com>
2023-09-24 04:45:48 +0700

    ProtectManga-mainCode-In-blog.js
    Kode yang dipasang di website

---
+++ ProtectManga-mainCode-In-blog.js
@@ -0,0 +1,7 @@
+<b:if cond='data:view.isPost'>
+<script defer='defer' fetchpriority='low'>/*<![CDATA[*/
+window.onload = function() {
+ProtectImageJS.protect(document.querySelectorAll("img"));
+}
+/*]]>*/</script>
+</b:if>
